9b430902dfbda405a492c9a16bcd312ba8585dc4
shikyo <xiaoxiaolantian@foxmail.com>
2016-12-13 16:01:02 +0800

    dataup

---
+++ 小项目/script/index.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+	
+	var smBoxs=document.querySelectorAll(".smbox");
+	var shadowBox=document.querySelector("#shadowBox");
+	var nav=document.querySelector(".nav");
+	var next=nav.querySelector(".next");
+	var prev=nav.querySelector(".prev");
+	var imgs=nav.querySelectorAll("img");
+
+	shadowBox.style.height=document.querySelector("html").offsetHeight+'px';
+
+	for(var i=0,l=smBoxs.length;i<l;i++){
+		smBoxs[i].index=i;
+		smBoxs[i].onclick=function(){
+			shadowBox.style.display='block';
+			nav.style.transform='scale(1)';
+			
+		imgs[1].src="images/work_"+this.index+"_big.jpg";	
+		}
+	}
+
+	shadowBox.onclick=function(){
+		this.style.display='none';
+		nav.style.transform='scale(0)';
+		nav.style.display='none';
+	}
+	
+	
+
+}
